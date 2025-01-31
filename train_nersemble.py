8744bf4bb758102ade9625d4ef925ed27010062c
vladb99 <vladut.bratulescu@gmail.com>
2025-01-31 13:51:06 +0100

    Add train and mesh extraction scripts for nersemble

---
+++ train_nersemble.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from tqdm import tqdm
+import os
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Script parameters")
+    parser.add_argument("--timesteps_folder", type=str)
+    parser.add_argument("--output_folder", type=str)
+    args = parser.parse_args()
+
+    timesteps_folders = sorted(os.listdir(args.timesteps_folder))
+
+    for index, timestep_folder in enumerate(tqdm(timesteps_folders)):
+        os.system("python train.py -s {} -m {} -r 2 -w".format(os.path.join(args.timesteps_folder, timestep_folder), os.path.join(args.output_folder, "timestep_" + str(index))))
